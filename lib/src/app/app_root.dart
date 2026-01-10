f093aa02211cb9e55f472b02b9f8e5cfd3d00886
Jonas Vestlie <snekkerjonas@gmail.com>
2026-01-10 12:26:14 +0100

    feat: update AppRoot to use StartScreen, StepScreen, CompletionScreen

--- lib/src/app/app_root.dart
+++ lib/src/app/app_root.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import '../ui/start_screen.dart';
+import '../ui/step_screen.dart';
+import '../ui/completion_screen.dart';
 import 'flow_coordinator.dart';
 
-class AppRoot extends StatelessWidget {
+class AppRoot extends StatefulWidget {
   final FlowCoordinator flow;
-
   const AppRoot({super.key, required this.flow});
 
   @override
+  State<AppRoot> createState() => _AppRootState();
+}
+
+class _AppRootState extends State<AppRoot> {
+  int screen = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: flow.start,
-            child: const Text('Start'),
-          ),
+    if (screen == 0) {
+      return MaterialApp(
+        home: StartScreen(
+          flow: widget.flow,
         ),
+      );
+    }
+
+    if (screen == 1) {
+      return const MaterialApp(
+        home: StepScreen(
+          stepText: 'Steg 1',
+          onNext: _noop,
+        ),
+      );
+    }
+
+    return const MaterialApp(
+      home: CompletionScreen(
+        onDone: _noop,
       ),
     );
   }
+
+  static void _noop() {}
 }
