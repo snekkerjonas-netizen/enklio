c84d24a77925925834329bc9b513cd139a569bde
Jonas Vestlie <snekkerjonas@gmail.com>
2026-01-10 12:51:35 +0100

    feat: StepScreen is now stateful with multiple steps

--- lib/src/ui/step_screen.dart
+++ lib/src/ui/step_screen.dart
@@ -3,18 +3,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import '../app/flow_coordinator.dart';
 
-class StepScreen extends StatelessWidget {
+class StepScreen extends StatefulWidget {
   final FlowCoordinator flow;
 
   const StepScreen({super.key, required this.flow});
 
   @override
+  State<StepScreen> createState() => _StepScreenState();
+}
+
+class _StepScreenState extends State<StepScreen> {
+  final List<String> steps = [
+    'Gjør klar arbeidsområdet',
+    'Utfør hovedoppgaven',
+    'Rydd opp',
+  ];
+
+  int index = 0;
+
+  void nextStep() {
+    if (index < steps.length - 1) {
+      setState(() => index++);
+    } else {
+      widget.flow.complete();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: flow.next,
-          child: const Text('Neste steg'),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              steps[index],
+              style: const TextStyle(fontSize: 22),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: nextStep,
+              child: Text(index < steps.length - 1 ? 'Neste' : 'Ferdig'),
+            ),
+          ],
         ),
       ),
     );
